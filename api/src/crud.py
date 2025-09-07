0385059e9aca4a8c418b626cef26f664def3e49f
Julien Moorat <contact@julienmoorat.fr>
2025-09-07 11:35:47 +0200

    feat(api): add advanced price history endpoint with detailed statistics

--- api/src/crud.py
+++ api/src/crud.py
@@ -75,19 +75,47 @@ def get_price_history(
     token_name: str,
     network: str,
     is_primary_market: bool,
+    advanced: bool,
     time_bucket: QueryableTimeBucket,
 ):
-    sql = text("""
-        SELECT
-            time_bucket(:time_bucket, timestamp) as time_bucket,
-            avg(price_eth) as price_eth,
-            round(avg(premium)*100, 3) as premium_percentage
-        FROM prices
-        WHERE timestamp > now() - INTERVAL :time_window
-        AND token_name = :token_name AND network = :network AND is_primary_market = :is_primary_market
-        GROUP BY time_bucket, token_name, network, is_primary_market
-        ORDER BY time_bucket DESC;
-    """)
+    if advanced:
+        sql = text("""
+           SELECT 
+               time_bucket(:time_bucket, timestamp) as time_bucket,
+               
+               min(price_eth) as min_price_eth,
+               max(price_eth) as max_price_eth,
+               avg(price_eth) as avg_price_eth,
+               first (price_eth, timestamp) as first_price_eth,
+               last (price_eth, timestamp) as last_price_eth,
+               
+               round(min (premium)*100, 3) as min_premium_percentage,
+               round(max (premium)*100, 3) as max_premium_percentage,
+               round(avg (premium)*100, 3) as avg_premium_percentage,
+               round(first (premium, timestamp)*100, 3) as first_premium_percentage,
+               round(last (premium, timestamp)*100, 3) as last_premium_percentage
+
+           FROM prices
+           WHERE timestamp
+               > now() - INTERVAL :time_window
+             AND token_name = :token_name
+             AND network = :network
+             AND is_primary_market = :is_primary_market
+           GROUP BY time_bucket, token_name, network, is_primary_market
+           ORDER BY time_bucket DESC;
+           """)
+    else:
+        sql = text("""
+            SELECT
+                time_bucket(:time_bucket, timestamp) as time_bucket,
+                avg(price_eth) as price_eth,
+                round(avg(premium)*100, 3) as premium_percentage
+            FROM prices
+            WHERE timestamp > now() - INTERVAL :time_window
+            AND token_name = :token_name AND network = :network AND is_primary_market = :is_primary_market
+            GROUP BY time_bucket, token_name, network, is_primary_market
+            ORDER BY time_bucket DESC;
+        """)
     result = db.execute(
         sql,
         {
