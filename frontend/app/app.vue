36049633948cc9babd2d0a11572da5bf2b4a2ad9
Julien Moorat <contact@julienmoorat.fr>
2025-11-18 23:05:33 +0100

    feat(frontend): add custom logo component and update header layout

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,9 +3,11 @@
     <UContainer class="py-12">
     <div class="space-y-10">
       <header class="text-center space-y-2">
-        <NuxtLink to="/" class="inline-block">
-          <Icon name="streamline:ethereum-circle-solid" class="w-12 h-12 text-primary-400 mb-2" />
-          <h1 class="text-3xl font-semibold text-primary-400">ETH LST Tracker</h1>
+        <NuxtLink to="/">
+          <div class="inline-flex flex-col items-center">
+            <Logo class="w-24 h-24 text-primary-400 mb-4" />
+            <h1 class="text-3xl font-semibold text-primary-400">ETH LST Tracker</h1>
+          </div>
         </NuxtLink>
         <p class="text-sm text-gray-400 max-w-2xl mx-auto">
           Track prices of the main Ethereum LSTs (Liquid Staking Tokens) and compare their premiums on secondary markets.
